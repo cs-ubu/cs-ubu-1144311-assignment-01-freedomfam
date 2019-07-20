ed76f901cd9888188cb1147479ff18da1d31c92d
freedomfam <33215329+freedomfam@users.noreply.github.com>
2019-07-20 21:39:05 +0700

    up

--- matmul.py
+++ matmul.py
@@ -1,29 +1,27 @@
-# import csv
+import numpy as np
+import csv
 
-# f = open('A.csv','r')
-# A =[]
-# for line in f.readline():
-#     A.append([ float(x) for x in line.strip().split(',')])
+'''def csv2mat(filename):
+    return np.array(list(csv.reader(open(filename, "r"), delimiter=",")))
+A=csv2mat('A.csv')
+b=csv2mat('b.csv')
+'''
 
-# print(A)
-# f.close()
-
-# f = open('b.csv','r')
-# b =[]
-# for line in f.readline():
-#     A.append([ float(x) for x in line.strip().split(',')])
-
-
+def readm(fname='A.csv'):
+    f= open(fname,'r')
+    A=[]
+    for line in f.readlines():
+        A.append([float(x) for x in line.strip().split(',')])
+    f.close()
+    return A
 
 def matmul(A,b):
-    m,n = len(A) , len(b[0])
-    J= len(A[0])
-    if len(A[0]) == len(b):
-        C = [ [0]*n for i in range(m) ]
+    m,n=len(A),len(b[0])
+    J = len(A[0])
+    if len(A[0])==len(b):
+        C=[ [0]*n for i in range(m)]
         for r in range(m):
             for c in range(n):
-                C[r][c] = sum([ A[c][j]*b[j][c] for j in range(J)])
-            return C
-        return []
-mat = matmul(A,b)
-print(mat)
+                C[r][c] = sum([float(A[r][j])*float(b[j][c]) for j in range(J)])
+        return C
+    return [] #can't matmul
